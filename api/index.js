9278be66647ce071039bda07b08a26a7dc603a95
Matheus Hernandes <midia.matheus@gmail.com>
2020-04-14 19:19:02 -0300

    video 3.2

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const Koa = require('koa')
+const processador = require('koa-bodyparser')
 const aplicacao = new Koa()
+const pesquisar = require('../bigquery/pesquisar')
 
-aplicacao.use(function (contexto) {
+aplicacao.use(processador())
+aplicacao.use(async function (contexto) {
+    const corpoDaRequisicao = contexto.request.body
     contexto.status = 200
-    contexto.body = {
-        mensagem: 'API está funcionando'
-    }
+    contexto.body = await pesquisar(corpoDaRequisicao.filtro)
 })
 
 aplicacao.listen(3000)

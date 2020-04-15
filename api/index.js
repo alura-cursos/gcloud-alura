1886091ed6c19fdbc3933315e497684495f06ecb
Matheus Hernandes <midia.matheus@gmail.com>
2020-04-15 16:26:13 -0300

    video 3.3

--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
 const Koa = require('koa')
+const conversor = require('basic-auth')
 const processador = require('koa-bodyparser')
 const aplicacao = new Koa()
 const pesquisar = require('../bigquery/pesquisar')
 
 aplicacao.use(processador())
 aplicacao.use(async function (contexto) {
+    const usuarioEsenha = conversor.parse(contexto.request.headers.authorization)
+    const USUARIO = process.env.USUARIO
+    const SENHA = process.env.SENHA
+    
+    if (USUARIO !== usuarioEsenha.name || SENHA !== usuarioEsenha.pass) {
+        contexto.body = {
+            mensagem: 'Acesso negado'
+        }
+        return
+    }
+
     const corpoDaRequisicao = contexto.request.body
     contexto.status = 200
     contexto.body = await pesquisar(corpoDaRequisicao.filtro)
